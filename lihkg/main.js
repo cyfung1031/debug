3531784effc823f455c56a80c4af853b6901f77f
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2021-07-22 17:14:43 +0800

    Add files via upload

---
+++ lihkg/main.js
@@ -0,0 +1,35 @@
+-(() => {
+let index = 0;
+let c = document.querySelector('.qoAmEqNpZRLf2KVKZ8DsC');
+let str = '';
+for (let i = 0; i < 60; i++) {
+
+    index++;
+    str += `
+<div class="wQ4Ran7ySbKd8PdMeHZZR">
+<div class="_21IQKhlBjN2jlHS_TVgI3l">
+    <div class="vv9keWAXpwoonDah6rSIU">
+        <div class="_3D2lzCKDMcdgEkexZrTSUh"></div>
+        <div class="zBQrhafI5njrmUUaJqKA4"><span class="CxY4XDSSItTeLVg0cKCN0 jj_3ZDzjtPixL1b2KTcpS">User${index}</span><span class="_37XwjAqVHtjzqzEtybpHrU">time${index}</span><span class="_37XwjAqVHtjzqzEtybpHrU"><i class="i-thumb-up"></i> 2</span></div>
+        <div class="_26oEXjfUS_iHzbxYcZE6bD">1 頁 <i class="i-menu-down"></i><select class="_3N7MXL_26Dhhcccm_q8AKa">
+                <option value="">選擇頁數</option>
+                <option value="123|1">1 頁</option>
+            </select></div>
+    </div>
+    <div class="vv9keWAXpwoonDah6rSIU _1KV65JcPoDUFZ4o3OEqn4R">
+        <div class="_3D2lzCKDMcdgEkexZrTSUh"></div><a class="_2A_7bGY9QAXcGu1neEYDJB" href="#abc332"></a>
+        <div class="_14BnVEj9rQ5VQKmrIqn9Qj"><span class="_20jopXBFHNQ9FUbcGHLcHH">Content${index}</span></div><a class="_3VRxq3mC-jm2fhdX4exwPU " href="#abc333">吹水台</a>
+    </div>
+</div>         
+</div>
+`;
+
+}
+
+c.innerHTML = str;
+
+}, true);
+
+
+
+})();
